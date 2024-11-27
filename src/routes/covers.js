30a02a326cbe5411a9b4025ccf31480263912eac
m-dantas <mauricio.dantascp@gmail.com>
2024-11-26 21:20:10 -0300

    teste: inclui trycatch para verificar erro

--- src/routes/covers.js
+++ src/routes/covers.js
@@ -4,10 +4,15 @@ const axios = require('axios')
 const route = Router()
 
 route.get('/:mangaId/:coverId', async (req, res) => {
+   try {
     const { mangaId, coverId } = req.params
     const response = await axios.get(`https://uploads.mangadex.org/covers/${mangaId}/${coverId}`, { responseType: 'arraybuffer' })
     res.setHeader('Content-Type', 'image/jpeg')
     return res.send(response.data)
+   } catch (error) {
+    console.log(error)
+    return res.send('Servidor não conseguiu processar')
+   }
 })
 
 module.exports = route
